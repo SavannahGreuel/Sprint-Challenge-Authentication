58177cd1dfb2ff7199299c9fe9db78dedd117589
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-30 09:53:45 -0700

    added herroku api

--- config/routes.js
+++ config/routes.js
@@ -43,12 +43,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      'https://safe-falls-22549.herokuapp.com/random_ten'
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: 'Error Fetching Jokes', err });
     });
 }
