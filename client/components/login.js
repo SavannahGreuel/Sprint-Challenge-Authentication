a8b5425ba8cc4d7910cc074bb7ec997006e4141f
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-30 10:13:19 -0700

    added login component

---
+++ client/components/login.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../index.css';
+const url = process.env.REACT_APP_API_URL;
+
+const initialUser = {
+    username: '',
+    password: '',
+}
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {...initialUser},
+            message: '',
+        }
+    }
+    
+    inputHandler = (event) => {
+        const { name, value } = event.target;
+        this.setState({ user: {...this.state.user, [name]: value}})
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        axios.post(`${url}/api/login`, this.state.user)
+            .then(res => {
+                if (res.status === 200 && res.data) {
+                    localStorage.setItem('secret_token', res.data.token)
+                    this.setState({
+                        message: 'Login Successful',
+                        user: {...initialUser },
+                    })
+                    this.props.history.push("/api/jokes");
+                } else {
+                   throw new Error();
+                }
+            })
+            .catch (err => {
+                this.setState ({
+                    message: 'Login Failed...',
+                    user: { ...initialUser},
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Sign In </h1>
+                <form onSubmit={this.submitHandler}>
+                <label htmlFor = 'username'> Username </label>
+                <input 
+                    type = 'text' 
+                    id='username'
+                    name='username' 
+                    value={this.state.username}
+                    onChange = {this.inputHandler}
+                />
+                <label htmlFor = 'password'> Password </label>
+                <input 
+                    type = 'text' 
+                    id='password'
+                    name='password' 
+                    value={this.state.password}
+                    onChange = {this.inputHandler}
+                 />
+        
+                 <button className ='submitButton'>Submit</button>
+                </form>
+                {this.state.message
+                    ?(<h4>{this.state.message}</h4>)
+                    : undefined
+                }
+            </div>
+        )
+    }
+}
